99cd6fb751b87c0d3741aa8adb02d56817e28aa8
Alexsey <alexrarder80740@gmail.com>
2026-03-10 18:00:25 +0300

    Lesson Changes

--- Assets/Scripts/Infrastructure/Loading.cs
+++ Assets/Scripts/Infrastructure/Loading.cs
@@ -16,18 +16,19 @@ public class Loading : MonoBehaviour
     private IEnumerator LoadSceneAsync(string sceneName)
     {
         gameObject.SetActive(true);
+
         m_image.fillAmount = 0;
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName)
+            ;
         var delta = 1 - m_image.fillAmount;
         const int steps = 10;
+
         for (var i = 0; i<10; i++)
         {
-
             yield return new WaitForSeconds(0.5f);
             m_image.fillAmount += delta/ steps;
-
         }
         yield return null;
     }
-
 }
